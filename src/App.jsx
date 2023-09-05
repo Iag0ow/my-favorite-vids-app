e949ccc8eef8f8a5da66e8d7fb736e93da782f66
Iag0ow <iagomacedo159@gmail.com>
2023-09-04 21:15:36 -0300

    tela de login finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Navigate,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
 import './App.css'
+import Login from './pages/login/Login';
+import Home from './pages/home/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const user = localStorage.getItem("token");
+  const [auth, setAuth] = useState(false);
+  const handleUpdateAuth = (value) => {
+    setAuth(value);
+  };
+  useEffect(() => {
+    if (user) {
+      setAuth(true);
+    }
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Login.css'
+import icon_register from '../../assets/images/mdi_register-outline.png'
+const Login = () => {
+  return (
+    <div className="login-page">
+          <h1 className="my-favorite-videos-title">My Favorite<span> Videos</span></h1>
+          <form className="container login-page-box">
+            <div className="row">
+              <div className='col-md-12'>
+                <h1>Entrar</h1>
+                <label className='mt-5'>Email</label>
+                <input className="form-control form-control-lg mt-3" type="text" name="email" placeholder="Seu Email" />
+                <label className='mt-4'>Password</label>
+                <input className="form-control form-control-lg mt-3" type="password" placeholder="Senha" />
+                <button type="submit" className="btn btn-entrar mt-4">Entrar</button>
+                <div className="text-center mt-4 mb-4 ou-border">
+                    <div></div>
+                    <span>ou</span>
+                    <div></div>
+                </div>
+                <button type="submit" className="btn btn-registrar text-center"><span>Registra-se</span></button>    
+              </div>
+            </div>
+          </form>
+    </div>
+  )
+}
+
+export default Login

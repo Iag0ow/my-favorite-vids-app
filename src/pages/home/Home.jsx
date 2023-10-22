f16d6da30dad7cf88fe93e80e5199cf81991d5d4
Iago <iagomacedo159@gmail.com>
2023-10-22 20:26:21 -0300

    Card com função de atualizar funcional

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,23 +1,61 @@
-import React, { useEffect, useState,useContext } from 'react'
-import './Home.css'
-import Aside from '../../components/Aside/Aside'
-import { deleteVideo, getAllVideos, getPlatforms, getVideoById } from "../../utils/config";
-import { useParams } from 'react-router-dom';
-import Swal from 'sweetalert2';
-import { useNavContext } from '../../context/NavBarInfContext';
-import ReactDOMServer from 'react-dom/server';
-const Home = () => {
-const { platformParam } = useParams();
-const {navBarData,updateComponentNav,updateNavPlatform,updateNavBarData  } = useContext(useNavContext);
+import React, { useEffect, useState, useContext } from "react";
+import Aside from "../../components/Aside/Aside";
+import "./Home.css";
+import {
+  deleteVideo,
+  getAllVideos,
+  getPlatforms,
+  getVideoById,
+  updateVideo,
+} from "../../utils/config";
+import { useParams } from "react-router-dom";
+import Swal from "sweetalert2";
+import { useNavContext } from "../../context/NavBarInfContext";
+import ReactDOMServer from "react-dom/server";
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
 
-  const [platform,setPlatform] = useState("");
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  color: 'white',
+  backgroundColor: 'black',
+  transform: 'translate(-50%, -50%)',
+  width: 695,
+  border: '2px solid #ffffff47',
+  boxShadow: 24,
+  borderRadius: 3,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+const responsiveStyle = {
+  '@media (max-width: 998px)': {
+    width: 351,
+  },
+}
+const Home = () => {
+  
+  const { platformParam } = useParams();
+  const {
+    navBarData,
+    updateComponentNav,
+    updateNavPlatform,
+    updateNavBarData,
+  } = useContext(useNavContext);
+
+  const [platform, setPlatform] = useState("");
   const [allVideos, setAllVideos] = useState([]);
   const [formData, setFormData] = useState({
     title: "",
     description: "",
-    url: "",
-  })
+    privy: "",
+  });
   const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [currentId, setCurrentId] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -27,39 +65,38 @@ const {navBarData,updateComponentNav,updateNavPlatform,updateNavBarData  } = use
       setLoading(false);
     }
     fetchData();
-  }, []);
+  }, [updateComponentNav]);
 
   useEffect(() => {
     async function fetchData() {
       const platforms = await getPlatforms();
       setPlatform(platforms[0]);
-      if(platformParam == undefined){
+      if (platformParam == undefined) {
         updateNavBarData(platforms[0]);
       }
     }
     fetchData();
-  })
+  }, []);
+
   const handleDelete = async (id) => {
     setLoading(true);
     Swal.fire({
-      title: 'Deseja realmente excluir o vídeo?',
-      text: 'Esta ação não pode ser desfeita!',
-      icon: 'warning',
+      title: "Deseja realmente excluir o vídeo?",
+      text: "Esta ação não pode ser desfeita!",
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonText: 'Excluir',
-      cancelButtonText: 'Cancelar',
+      confirmButtonText: "Excluir",
+      cancelButtonText: "Cancelar",
       customClass: {
-        popup: 'custom-swal-popup',
-        confirmButton: 'custom-swal-confirm-button',
-        cancelButton: 'custom-swal-cancel-button',
+        popup: "custom-swal-popup",
+        confirmButton: "custom-swal-confirm-button",
+        cancelButton: "custom-swal-cancel-button",
       },
-    })
-    .then(async (result) => {
+    }).then(async (result) => {
       if (result.isConfirmed) {
         const data = await deleteVideo(id);
-  
-        if (data.hasOwnProperty("error")) {
 
+        if (data.hasOwnProperty("error")) {
           setLoading(false);
           setMessageFail(true);
 
@@ -69,145 +106,149 @@ const {navBarData,updateComponentNav,updateNavPlatform,updateNavBarData  } = use
           return;
         }
 
-          Swal.fire({
-            title: 'Excluído!',
-            text: 'O vídeo foi excluído com sucesso.',
-            icon: 'success',
-            customClass: {
-              popup: 'custom-swal-popup-success',
-              confirmButton: 'custom-swal-confirm-button-success',
-            },
-          }).then(async () => {
-            const videos = await getAllVideos();
-            setAllVideos(videos);
-            if(updateComponentNav == true){
-              updateNavPlatform(false);
-            }else {
-              updateNavPlatform(true);
-            }
-          });
+        Swal.fire({
+          title: "Excluído!",
+          text: "O vídeo foi excluído com sucesso.",
+          icon: "success",
+          customClass: {
+            popup: "custom-swal-popup-success",
+            confirmButton: "custom-swal-confirm-button-success",
+          },
+        }).then(async () => {
+          const videos = await getAllVideos();
+          setAllVideos(videos);
+          if (updateComponentNav == true) {
+            updateNavPlatform(false);
+          } else {
+            updateNavPlatform(true);
+          }
+        });
       }
     });
+    setLoading(false);
   };
-  
+
+
   const handleUpdate = async (id) => {
     const currentVideo = await getVideoById(id);
-    setFormData(currentVideo);
-    const htmlContent = (
-      <div className="container">
-        <div className="form-group">
-          <label htmlFor="title">Título:</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            className="form-control"
-            value={formData.title}
-            onChange={handleInputChange}
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="description">Descrição:</label>
-          <input
-            type="text"
-            id="description"
-            name="description"
-            className="form-control"
-            value={formData.description}
-            onChange={handleInputChange}
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="privy">Privy:</label>
-          <input
-            type="text"
-            id="privy"
-            name="privy"
-            className="form-control"
-            value={formData.privy}
-            onChange={handleInputChange}
-          />
-        </div>
-      </div>
-    );
-    Swal.fire({
-      title: 'Editar Item',
-      html: ReactDOMServer.renderToString(htmlContent),
-      confirmButtonText: 'Salvar',
-      showCancelButton: true,
-    });
-    // setLoading(true);
-    // const body ={
-    //   title: document.getElementById("title").value,
-    //   description: document.getElementById("description").value,
-    //   privy: document.getElementById("privy").value,
-    // }
-    // const data = await updateVideo(id,body);
-    // // if (data.hasOwnProperty("error")) {
-    // //   setMessageFail(true);
-    // //   setTimeout(() => {
-    // //     setMessageFail(false);
-    // //   }, 3000);
-    // //   return;
-    // // }
-    // setMessageUpdate(true);
-    // setLoading(false);
-  }
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
+    setCurrentId(id);
     setFormData({
-      ...formData,
-      [name]: value,
+      title: currentVideo.title,
+      description: currentVideo.description,
+      privy: currentVideo.privy,
     });
+    setOpen(true);
   };
-  return (
-    <div className='home'>
-      <Aside />
-      <div className='container'>
-          <h2 className='mb-4'>Galeria</h2>
-        <div className='row'>
-        {allVideos.data && allVideos.data.map((video, index) => (
-          platformParam === video.platform && (
-            <div key={index} className='col-md-4 col-lg-3 mb-3 card-video'>
-            <iframe
-              width="100%"
-              height="550"
-              src={video.url}
-              title="YouTube video player"
-              allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            ></iframe>
-            <p className='text-center'>{video.title}</p>
-            <hr />
-            <div className='d-flex justify-content-center align-items-center  mb-3'>
-              <i onClick={() => handleUpdate(video._id)} className="fa-solid me-5 fa-pen fa-2x" data-bs-toggle="modal" data-bs-target="#customerModal"></i>
-              <i onClick={() => handleDelete(video._id)} className="fa-solid fa-trash fa-2x"></i>
-            </div>
-          </div>
-          )
-        ))}
-        {allVideos.data  &&  platformParam == undefined && allVideos.data.map((video, index) => (
-          platform === video.platform && (
-            <div key={index} className='col-md-4 col-lg-3 mb-3 card-video'>
-            <iframe
-              width="100%"
-              height="550"
-              src={video.url}
-              title="YouTube video player"
-              allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            ></iframe>
-            <p className='text-center'>{video.title}</p>
-            <hr />
-            <div className='d-flex justify-content-center align-items-center  mb-3'>
-              <i onClick={() => handleUpdate(video._id)} className="fa-solid me-5 fa-pen fa-2x" data-bs-toggle="modal" data-bs-target="#customerModal"></i>
-              <i onClick={() => handleDelete(video._id)} className="fa-solid fa-trash fa-2x"></i>
-            </div>
-            </div>
-          )
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleSave = async () => {
+    setLoading(true);
+    const result = await updateVideo(currentId, formData);
+    if (result.hasOwnProperty("error")) {
+      // setTimeout(() => {
+      //   setMessageErrorStatus(false);
+      // }, 6000);
+      return;
+    }
 
-        )
-        )}
+    if (updateComponentNav == true) {
+      updateNavPlatform(false);
+    } else {
+      updateNavPlatform(true);
+    }
+
+    setTimeout(() => {
+      setMessage(false);
+      setFormData({
+        title: "",
+        description: "",
+        privy: "",
+      });
+      setCurrentId("");
+    }, 2000);
+
+    setLoading(false);
+    setOpen(false);
+
+    Swal.fire({
+      title: "Sucesso!",
+      text: "Video atualizado.",
+      icon: "success",
+      customClass: {
+        popup: "custom-swal-popup-success",
+        confirmButton: "custom-swal-confirm-button-success",
+      },
+    })
+  }
+
+  return (
+    <div className="home">
+      <Aside />
+      <div className="container">
+        <h2 className="mb-4">Galeria</h2>
+        <div className="row">
+          {allVideos.data &&
+            allVideos.data.map(
+              (video, index) =>
+                platformParam === video.platform && (
+                  <div
+                    key={index}
+                    className="col-md-4 col-lg-3 mb-3 card-video"
+                  >
+                    <iframe
+                      width="100%"
+                      height="550"
+                      src={video.url}
+                      title="YouTube video player"
+                      allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    ></iframe>
+                    <p className="text-center">{video.title}</p>
+                    <hr />
+                    <div className="d-flex justify-content-center align-items-center  mb-3">
+                      <i
+                        onClick={() => handleUpdate(video._id)}
+                        className="fa-solid me-5 fa-pen fa-2x"
+                      ></i>
+                      <i
+                        onClick={() => handleDelete(video._id)}
+                        className="fa-solid fa-trash fa-2x"
+                      ></i>
+                    </div>
+                  </div>
+                )
+            )}
+          {allVideos.data &&
+            platformParam == undefined &&
+            allVideos.data.map(
+              (video, index) =>
+                platform === video.platform && (
+                  <div
+                    key={index}
+                    className="col-md-4 col-lg-3 mb-3 card-video"
+                  >
+                    <iframe
+                      width="100%"
+                      height="550"
+                      src={video.url}
+                      title="YouTube video player"
+                      allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    ></iframe>
+                    <p className="text-center">{video.title}</p>
+                    <hr />
+                    <div className="d-flex justify-content-center align-items-center  mb-3">
+                      <i
+                        onClick={() => handleUpdate(video._id)}
+                        className="fa-solid me-5 fa-pen fa-2x"
+                      ></i>
+                      <i
+                        onClick={() => handleDelete(video._id)}
+                        className="fa-solid fa-trash fa-2x"
+                      ></i>
+                    </div>
+                  </div>
+                )
+            )}
 
           {/* <div className='col-md-4 col-lg-3 mb-3 card-video'>
             <iframe width="100%" height="550" src="https://www.youtube.com/embed/dU_UKLnvD48?autoplay=0&loop=1" title="YouTube video player" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
@@ -220,8 +261,35 @@ const {navBarData,updateComponentNav,updateNavPlatform,updateNavBarData  } = use
           </div> */}
         </div>
       </div>
+      <div>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="parent-modal-title"
+          aria-describedby="parent-modal-description"
+          className="mb-3"
+        >
+          <Box sx={{ ...style, ...responsiveStyle, width: 510, paddingBottom: 7 }}>
+            <h3 id="parent-modal-title" className="pt-4 text-center">Editar Vídeo</h3>
+            {/* {errorPassword.length > 0 && errorPassword.map((item, i) => <h5 key={i} className="text-success text-danger mt-3 text-center">{item}</h5>)}
+            {messageDelete && <h5 className="text-success text-danger mt-3">As senhas estão divergentes</h5>} */}
+            <label className='mt-4'>Título</label>
+            <input className="form-control form-control-lg mt-3" type="text" name="Título" placeholder="Titulo" value={formData.title} onChange={(e)=> setFormData({ ...formData, title:e.target.value })} />
+            <label className='mt-4'>Descrição</label>
+            <input className="form-control form-control-lg mt-3" type="text" name="Descrição" placeholder="Descrição" value={formData.description} onChange={(e) => setFormData({ ...formData, description: e.target.value })} />
+            <label className="mt-3">
+                <div>Privacidade</div>
+                <select className="privacy-select" name="privacidade" value={formData.privy} onChange={(e) => setFormData({ ...formData, privy: e.target.value })}>
+                  <option value="true">Privado</option>
+                  <option value="false">Público</option>
+                </select>
+                </label>
+            <button className={`btn btn-alterar-video mt-4 bg-danger text-white w-100 ${loading ? 'disabled-link' : ''}`} onClick={handleSave}>{loading ? 'Salvando...' : 'Salvar' }</button>
+          </Box>
+        </Modal>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

39b6114d0d8c913e1b2cca449778b6b6a9ed97a4
silasoliv39 <silasoliv39@gmail.com>
2023-11-09 21:06:23 -0300

    fix: refact api client

---
+++ src/utils/custom-fetch.jsx
@@ -0,0 +1,45 @@
+import { logOut } from "./config";
+
+export const customFetch = async (url, options) => {
+  const token = localStorage.getItem("token");
+  if (!token) return;
+
+  try {
+	const headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+    }
+
+    const response = await fetch(url, { ...options, headers });
+
+    if (response.status === 401) return redirectToLogin();
+
+    return response.json();
+  } catch (error) {
+    return null;
+  }
+};
+
+export const customFetchNoAuth = async (url, options) => {
+  const token = localStorage.getItem("token");
+  if (!token) return;
+
+  try {
+    const headers = {
+      "Content-Type": "application/json",
+    }
+
+    const response = await fetch(url, { ...options, headers });
+
+    if (response.status === 401) return redirectToLogin();
+
+    return response.json();
+  } catch (error) {
+    return null;
+  }
+};
+
+export const redirectToLogin = () => {
+  logOut();
+  window.location.href = "/login";
+};
